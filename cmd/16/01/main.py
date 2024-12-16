502624f40101f13f215169fb6b141dfcd4b72b72
Nicholas Cheng <nchengyeeshen@gmail.com>
2024-12-16 20:14:11 +0800

    Day 16, part 1
    Djikstra's algorithm.

--- cmd/16/01/main.py
+++ cmd/16/01/main.py
@@ -1,5 +1,6 @@
 import sys
-from collections import deque
+import heapq
+from dataclasses import dataclass, field
 
 contents = open(sys.argv[1] if sys.argv[1] != "-" else 0).read()
 
@@ -16,45 +17,44 @@ for r, row in enumerate(contents.splitlines()):
             end = (r, c)
     grid.append(buf)
 
-path = None
+
+@dataclass(order=True)
+class Item:
+    cost: int
+    r: int = field(compare=False)
+    c: int = field(compare=False)
+    direction: tuple[int, int] = field(compare=False)
+    path: list[tuple[int, int]] = field(compare=False)
 
 
-def shortest_path(grid, start, end):
-    visited = set()
-    q = deque()
-    q.append((start, []))
-    paths = []
+items = {start: Item(0, *start, (0, 1), [])}
+q = [items[start]]
 
-    while len(q) > 0:
-        (r, c), path = q.popleft()
-        visited.add((r, c))
-        if (r, c) == end:
-            paths.append(path[:])
+while len(q) > 0:
+    item = heapq.heappop(q)
+    if (item.r, item.c) == end:
+        break
+
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        new_cost = item.cost + 1
+        if (dr, dc) != item.direction:
+            new_cost += 1000
+
+        nr, nc = item.r + dr, item.c + dc
+        if grid[nr][nc] == "#" or (item.path and (nr, nc) == item.path[-1]):
             continue
 
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nr, nc = r + dr, c + dc
+        if (nr, nc) in items:
+            next_item = items[(nr, nc)]
+            if new_cost < next_item.cost:
+                next_item.cost = new_cost
+                heapq.heapify(q)
+                next_item.path = item.path[:] + [(item.r, item.c)]
+        else:
+            new_item = Item(
+                new_cost, nr, nc, (dr, dc), item.path[:] + [(item.r, item.c)]
+            )
+            items[(nr, nc)] = new_item
+            heapq.heappush(q, new_item)
 
-            if grid[nr][nc] == "#" or (nr, nc) in path:
-                continue
-
-            q.append(((nr, nc), path[:] + [(r, c)]))
-
-    return paths
-
-
-def score(path):
-    curr_dir = (0, 1)
-    turns = 0
-    for (pr, pc), (cr, cc) in zip(path, path[1:]):
-        dr, dc = cr - pr, cc - pc
-        turned = (dr, dc) != curr_dir
-        curr_dir = (dr, dc)
-        if turned:
-            turns += 1
-    return len(path) + turns * 1000
-
-
-best_path = min(shortest_path(grid, start, end), key=score)
-
-print(score(best_path))
+print(items[*end].cost)
